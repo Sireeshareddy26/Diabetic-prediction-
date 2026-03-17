60ddb2bdedc0daa0e123d9245a48889eca0fe9e6
Sireeshareddy26 <138138762+Sireeshareddy26@users.noreply.github.com>
2026-03-17 11:09:23 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import streamlit as st
 import pandas as pd
 import pickle
 import numpy as np
-import sklearn # Explicitly import sklearn
+
 from sklearn.ensemble import RandomForestClassifier # Explicitly import the model class
 
 st.set_page_config(page_title="Diabetic Risk Predictor", layout="centered")
